02ed5327654b35c41ece8af72a3fe334e9121c4f
leandro2017 <leanceli@gmail.com>
2022-07-31 23:04:04 -0300

    test

--- src/main/java/com/primerApp/SprintBoot/SprintBootApplication.java
+++ src/main/java/com/primerApp/SprintBoot/SprintBootApplication.java
@@ -4,8 +4,8 @@ package com.primerApp.SprintBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 
@@ -17,18 +17,18 @@ public class SprintBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SprintBootApplication.class, args);
 	}
-@Bean
-        public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-           @Override
-           public void addCorsMappings(CorsRegistry registry) {
-               registry.addMapping("/**")
-               .allowedOrigins("*")
-               .allowedMethods("*")
-               .allowedHeaders("*");
-           }
-        };
-    }
+//@Bean
+//        public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//           @Override
+//           public void addCorsMappings(CorsRegistry registry) {
+//               registry.addMapping("/**")
+//               .allowedOrigins("*")
+//               .allowedMethods("*")
+//               .allowedHeaders("*");
+//           }
+//        };
+//    }
 
 
 }
