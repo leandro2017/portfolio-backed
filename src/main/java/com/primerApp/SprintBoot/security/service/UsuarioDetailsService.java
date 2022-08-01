075129e26cf07130d287bb47ab8cfb287b8f6cf5
leandro2017 <leanceli@gmail.com>
2022-07-31 23:26:07 -0300

    test

--- src/main/java/com/primerApp/SprintBoot/security/service/UsuarioDetailsService.java
+++ src/main/java/com/primerApp/SprintBoot/security/service/UsuarioDetailsService.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import java.util.Map;
-import lombok.var;
+
 
 @Service
 public class UsuarioDetailsService implements UserDetailsService  {
