e7df449803643103abb142d3549af8666f6bcfe2
leandro2017 <leanceli@gmail.com>
2022-07-31 23:15:37 -0300

    test

--- src/main/java/com/primerApp/SprintBoot/security/service/JwtUtilService.java
+++ src/main/java/com/primerApp/SprintBoot/security/service/JwtUtilService.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import lombok.var;
 
 @Service
 public class JwtUtilService {
